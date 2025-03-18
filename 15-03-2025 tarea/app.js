c93ac7db68818e030615e6cd012a80cb284a716f
Adrian Perez Zalazar <adrianperezzalazar@CLAP2009-1698.local>
2025-03-17 20:55:40 -0600

    tarea portotipos del objeto Array

---
+++ 15-03-2025 tarea/app.js
@@ -0,0 +1,54 @@
+let tablabody = document.getElementById('cuerpotabla')
+
+const arreglo1 = ['a','b','c','d','e','f','g','h','i','j','k','l'];
+const arreglo2 = ['a','b','c','d','e','f','g','h','i','j','k','l'];
+
+let ej_at = arreglo1.at(5);
+console.log(ej_at);
+
+let ej_concat = arreglo1.concat(arreglo2);
+console.log(ej_concat);
+
+let ej_copyWithin = arreglo1.copyWithin(1,3);
+console.log(ej_copyWithin);
+
+
+
+arreglo1.entries();
+arreglo1.every();
+arreglo1.fill();
+arreglo1.filter();
+arreglo1.find();
+arreglo1.findIndex();
+arreglo1.findLast();
+arreglo1.findLastIndex();
+arreglo1.flat();
+arreglo1.flatMap();
+arreglo1.forEach();
+arreglo1.includes();
+arreglo1.indexOf();
+arreglo1.join();
+arreglo1.keys();
+arreglo1.lastIndexOf();
+arreglo1.map();
+arreglo1.pop();
+arreglo1.push();
+arreglo1.reduce();
+arreglo1.reduceRight();
+arreglo1.reverse();
+arreglo1.shift();
+arreglo1.slice();
+arreglo1.some();
+arreglo1.sort();
+arreglo1.splice();
+arreglo1.toLocaleString();
+arreglo1.toReversed();
+arreglo1.toSorted();
+arreglo1.toSpliced();
+arreglo1.toString();
+arreglo1.unshift();
+arreglo1.values();
+arreglo1.with();
+arreglo1.length();
+
+
