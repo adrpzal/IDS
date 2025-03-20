795818aa0c47445a9ada7d6297cf7e17f5687ffe
Adrian Perez Zalazar <adrianperezzalazar@CLAP2009-1698.local>
2025-03-19 21:27:17 -0600

    tarea 2 19marzo

---
+++ 19-03-2025 t2/calc.js
@@ -0,0 +1,8 @@
+// export default { suma,resta, producto, division, residuo, potencia}
+
+export const suma = (a,b) => a+b;
+export const resta = (a,b) => a-b;
+export const producto = (a,b) => a*b;
+export const division = (a,b) => a/b;
+export const residuo = (a,b) => a%b;
+export const potencia = (a,b) => Math.pow(a,b);
