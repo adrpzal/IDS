6a65622940bec8751385dbc528ec6fdb59038d13
Adrian Perez Zalazar <adrianperezzalazar@CLAP2009-1698.local>
2025-03-21 16:55:45 -0600

    tarea 20martemplate

---
+++ 20-03-2025 t0/myCarousel.js
@@ -0,0 +1,69 @@
+export class MyCarousel extends HTMLElement{
+
+    constructor(){
+        super();
+        this.arrayimg = [];
+        this.contador = 0;
+        this.sizeh = null;
+        this.sizev = null;
+    }
+    
+    
+    static observedAttributes = ['images', 'sizeh', 'sizev']
+
+    connectedCallback(){
+        const shadow = this.attachShadow({mode: 'open'}) //generar un sub arbol del DOM
+        
+        // añadir css
+        const linkElem = document.createElement("link");
+        linkElem.setAttribute("rel", "stylesheet");
+        linkElem.setAttribute("href", "style.css");
+
+        let plantilla = document.querySelector('#plantilla')
+        let clone = plantilla.content.cloneNode(true)
+
+        // this.shadowRoot.innerHTML = clone;
+
+        
+        shadow.appendChild(linkElem);
+        shadow.appendChild(clone);
+
+        //////////////////
+        ////listeners/////
+        shadow.querySelector('#prev').addEventListener('click', () =>{   
+            if (this.contador===0) {
+                btn2.removeAttribute('disabled')
+                btn1.setAttribute('disabled', 'true')
+            }
+            if (this.contador > 0 ) {                 
+                img.src = this.arrayimg[this.contador-1]
+                this.contador --;
+            }
+        })  
+    
+        shadow.querySelector('#next').addEventListener('click', () =>{
+            if (this.contador===this.arrayimg.length-1) {
+                btn2.setAttribute('disabled', 'true')
+                btn1.removeAttribute('disabled')
+            }
+            if (this.contador< this.arrayimg.length-1) {           
+                img.src = this.arrayimg[this.contador+1]
+                this.contador ++
+            }
+        })
+         
+    }
+
+    disconnectedCallback(){}
+    adoptedCallback(){ }
+
+    attributeChangedCallback(name, odlValue, newValue){
+        if(name === 'images'){
+            this.arrayimg = JSON.parse(newValue);
+            console.log('parseado')
+            console.log(this.arrayimg)
+        }
+        if (name === 'sizeh') { this.sizeh = parseInt(newValue) }
+        if (name === 'sizev') { this.sizev= parseInt(newValue)  }
+    }
+}
